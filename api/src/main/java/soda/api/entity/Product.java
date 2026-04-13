1ee057e97c55506b68dd6bfc874cd8bf86c04e91
Mauricio <mauarguello@gmail.com>
2026-04-13 11:08:02 -0600

    Descripción de los cambios

--- api/src/main/java/soda/api/entity/Product.java
+++ api/src/main/java/soda/api/entity/Product.java
@@ -2,16 +2,16 @@
 package soda.api.entity;
 import jakarta.persistence.*;
 import lombok.*;
-@Entity @Table(name = "products")
+@Entity @Table(name = "produ")
 @Getter @Setter
 @NoArgsConstructor @AllArgsConstructor
 public class Product {
 @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 private Integer id;
-@Column(name="description", nullable = false)
+@Column(name="descrip", nullable = false)
 private String description;
 @Column(name="price", nullable = false)
 private double price;
-@ManyToOne() @JoinColumn(name = "id_category")
+@ManyToOne() @JoinColumn(name = "id_cate")
 private Category category;
 }
